70e8b4d2935da5d61769495454657d6b9cf1422e
nic1993 <534222412@qq.com>
2018-09-27 17:51:46 +0800

    2018.9.27.request response

---
+++ src/ex02/pymont/PrimitiveServlet.java
@@ -0,0 +1,50 @@
+package ex02.pymont;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class PrimitiveServlet implements Servlet{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init");
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("from service");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Hello. Roses are red");
+		out.print("Violets are blue");
+		
+		System.out.println(1234);
+	}
+
+}
